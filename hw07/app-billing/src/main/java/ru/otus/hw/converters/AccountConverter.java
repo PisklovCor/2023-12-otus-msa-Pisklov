a957b90e2dc405891457c7539a18ea6871399ad0
Andrey <andreypisklov@gmail.com>
2024-05-21 12:39:20 +0300

    [hw07]: рандомная генерация UUID

--- hw07/app-billing/src/main/java/ru/otus/hw/converters/AccountConverter.java
+++ hw07/app-billing/src/main/java/ru/otus/hw/converters/AccountConverter.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Component;
 import ru.otus.hw.dto.AccountDto;
 import ru.otus.hw.models.Account;
 
+import java.util.UUID;
+
 @Component
 public class AccountConverter {
     public Account mapDtoToModel(AccountDto dto) {
         Account account = new Account();
         account.setLogin(dto.getLogin());
-        account.setInvoice(dto.getInvoice());
+        account.setInvoice(UUID.randomUUID());
         account.setMoney(dto.getMoney());
         account.setFullName(dto.getFullName());
         return account;
